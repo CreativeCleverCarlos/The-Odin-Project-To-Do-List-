4f57eebd34f22313ee2b7dc577d8116961313532
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-03-18 16:10:05 -0400

    cleaning up index.js notes

--- src/index.js
+++ src/index.js
@@ -5,28 +5,19 @@ import './default.js'
 import Icon from './placeholder5.jpg';
 import defaultMenu from './default.js';
 import navWaitingOnClicks from './navigatingthepages.js';
+import todayPage from './todaypage.js';
 
 //a function that creates a div, and places the cat image into it
 function component(){
 
 
-    // can likely delete the "hello world, from webpack index.js example and the image to clean up code"
-    const contentDiv = document.querySelector(".content")
-    const test = document.createElement("h1");
-    test.textContent = "Hello World, from webpack Index.js"
-    // contentDiv.appendChild(test) //places the test content into the ui
-
 
     const element = document.createElement('div')
-    const aPicture = new Image();
-    aPicture.src = Icon
-    //removes the example picture below
-    //element.appendChild(aPicture) //places the picture into the dom
-
-    //test that the default.js is working
-    defaultMenu()
-    putUpNav()
-    navWaitingOnClicks()
+   
+    defaultMenu();
+    putUpNav();
+    navWaitingOnClicks();
+    todayPage();
 
 
     return element
